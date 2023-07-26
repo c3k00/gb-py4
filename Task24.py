cbd486145414fbf925ee5e04645bc85573029193
Artsiom <c3k00@proton.me>
2023-07-26 20:44:18 +0300

    task 22

--- Task24.py
+++ Task24.py
@@ -10,14 +10,14 @@
 
 
 
-n=int(input("Введите количество  кустов:"))
-arr=list()
+n = int(input("Введите количество  кустов:"))
+arr = list()
 for i in range(n):
-    x=int(input("Введите количество ягод:"))
+    x = int(input("Введите количество ягод:"))
     arr.append(x)
-    arr_count=list()
-for i in range(len(arr)-1):
-    arr_count.append(arr[i-1]+arr[i]+arr[i+1])
-arr_count.append(arr[-2]+arr[-1]+arr[0])
+    arr_count = list()
+for i in range(len(arr) -1):
+    arr_count.append(arr[i -1] + arr[i] + arr[i + 1])
+arr_count.append(arr[-2] + arr[-1] + arr[0])
 print(arr)
 print("Максимальное число ягод за один заход =", max(arr_count))  
