40566bf745e3d00c970405b66547b269d9604e16
Artsiom <c3k00@proton.me>
2023-07-26 20:47:16 +0300

    task 22.1

--- Task22.py
+++ Task22.py
@@ -4,15 +4,6 @@
 # Затем пользователь вводит сами элементы множеств.
 
 
-# from random import randint
-# n_set = set(randint(1, 20) for i in range(int(input('Введите кол-во элементов первого множества: '))))
-# print(n_set)
-# m_set = set(randint(1, 20) for i in range(int(input('Введите кол-во элементов второго множества: '))))
-# print(m_set)
-# s_set = sorted(n_set.intersection(m_set))
-# print(*s_set)
-
-
 n, m = input('Введите два числа: ').split()
 first = [int(i) for i in input('Введите кол-во элементов первого множества: ').split()]
 second = [int(j) for j in input('Введите кол-во элементов второго множества: ').split()]
